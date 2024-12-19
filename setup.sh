4e47d6f003112eb5fc930f641248ef225c8e8c29
Norataisen <142599223+NoraTaiSen@users.noreply.github.com>
2024-12-19 20:34:12 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,6 +10,3 @@ pip install requests Flask colorama aiohttp psutil crypto pycryptodome loguru se
 curl -Ls "https://raw.githubusercontent.com/NoraTaiSen/setup/refs/heads/main/Rejoin.py" -o /sdcard/Download/Rejoin.py
 
 echo "Cài đặt và tải file hoàn tất!"
-
-# Chạy lệnh su để thực thi Python script sau khi setup
-su -c "cd /sdcard/Download && export PATH=\$PATH:/data/data/com.termux/files/usr/bin && export TERM=xterm-256color && python ./Rejoin.py"
